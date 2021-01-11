0526b686a2a54d10e86af75a44a694c3bea4d13c
Tetsuo <tetsuocwb@gmail.com>
2021-01-11 20:00:17 -0300

    update

--- testetemp/webtimer.py
+++ testetemp/webtimer.py
@@ -7,18 +7,21 @@ from kivy.clock import Clock
 import time
 import datetime as dt
 from datetime import datetime
+import pyautogui
 
 inicio = datetime.now()
 current = None
 elapsed = time.strftime('%I'+':'+'%M'+':'+'%S'+' %p')
-
+pausa = 0
 class relogioApp(App):
-    displayText = ObjectProperty() 
+    displayText = ObjectProperty()
     def build(self):
+        global event
         b=Base()
         c=b.ids.displayText
-        Clock.schedule_interval(c.update, 1)
+        event  = Clock.schedule_interval(c.update, 1)
         #Display.text= "w"
+        event.cancel()
         return b
 
 
@@ -26,10 +29,29 @@ class Base(BoxLayout):
     def start(self):
         global inicio
         inicio = datetime.now()
-        print ( 'daf')
+        print('daf')
+
+    def stop(self):
+        global inicio
+        # time.sleep(5)
+        # #pyautogui.click('/Users/tetsuo/dev/camera.png')
+        # print(pyautogui.locateOnScreen('/Users/tetsuo/dev/camera.png', confidence=.9))
+        # cam=pyautogui.locateOnScreen('/Users/tetsuo/dev/camera.png', confidence=.9)
+        # x1= cam.left+25
+        # y1= cam.top+25
+        # #pyautogui.click('/Users/tetsuo/dev/camera.png', confidence=.5)
+        # pyautogui.click(x=x1,y=y1)
+        print('acabou')
+        event()
+
+
+    def reset(self):
+        global inicio
+        inicio = datetime.now()
+        print('daf')
 
 class Display(Label):
-    
+    global pausa
     def update(self, *args):
         segundos = int((datetime.now()-inicio).total_seconds())
         minutos = segundos // 60
@@ -37,11 +59,19 @@ class Display(Label):
         print(minutos)
         print(self.diferenca(0,15,0,horas,minutos,segundos))
         self.text= str(self.diferenca(0,15,0,horas,minutos,segundos))[2:]
+        # if self.diferenca(0, 0, 20,horas,minutos,segundos)[2:] < 0:
+        if int(self.diferenca(0, 0, 10,horas,minutos,segundos).days)<0:
+            pausa = 1
+            # Base.stop(self)
+            #relogioApp.build(self).event.cancel()
+        print(
+        int(self.diferenca(0, 0, 20,horas,minutos,segundos).days)<0
+        )
 
         return
 
-    def diferenca(self, h1,m1,s1,h2,m2,s2):
-                
+    def diferenca(self, h1, m1, s1, h2, m2, s2):
+
         start_time = dt.time(h1, m1, s1)
         stop_time = dt.time(h2, m2, s2)
 
